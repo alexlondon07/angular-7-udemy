2eaa20c47d88ef9383aba38cb93f9d0a5f6aff11
Alexander Londoño Espejo <alexlondon07@gmail.com>
2018-12-26 09:45:28 -0500

    La ruta actual (routerLinkActive)

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
-
+  title = 'Bienvenido a NGZOO';
   constructor() { }
 
   ngOnInit() {
