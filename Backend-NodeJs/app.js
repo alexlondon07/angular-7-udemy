21bef75ac26fc88d6ccc2f8ebb475a980213a088
Alexander Londoño Espejo <alexlondon07@gmail.com>
2019-03-10 11:59:54 -0500

    CORS

--- Backend-NodeJs/app.js
+++ Backend-NodeJs/app.js
@@ -14,6 +14,13 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 //Config Headers and Cors 
+app.use((req, res, next) =>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+})
 
 //Base routes
 app.use('/api', user_routes);
