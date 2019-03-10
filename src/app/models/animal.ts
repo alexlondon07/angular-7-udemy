45b0d05d2c5bcaef944bdb065ba3de2f99b85fca
Alexander Londoño Espejo <alexlondon07@gmail.com>
2019-03-10 13:43:21 -0500

    Se agregan modelos de user y animal, Recibir los datos del formulario de registro

---
+++ src/app/models/animal.ts
@@ -0,0 +1,12 @@
+export class Animal{
+
+    constructor(
+        public name: string,
+        public description: string,
+        public year: string,
+        public image: string,
+        public user: string
+    ){
+
+    }
+}
--- src/components/register/register.component.ts
+++ src/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, Params, ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'register',
@@ -8,13 +9,19 @@ import { Router, Params, ActivatedRoute } from '@angular/router';
 export class RegisterComponent implements OnInit {
 
   public title: string;
-  
+  public user: User;
+
   constructor(private _route : ActivatedRoute, private _router: Router) { 
     this.title = 'Register';
+    this.user = new User('','','','','','ROLE_USER', '');
   }
 
   ngOnInit() {
     console.log('Register Component Loaded !!');
+  }
+
+  onSubmit(){
+    console.log(this.user);
   }
 
 }
